12a98b93f4d25759d4c1a69e662178bbdab05853
E <ehsaanzxk@eroam-114-61.wifi.wmin.ac.uk>
2026-03-16 19:40:22 +0000

    added to the app url settings and fixed the file so that organisation has a html file

--- sky_engineering/urls.py
+++ sky_engineering/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("messages_feature.urls")),
+    path("",include("organisation_feature.urls")),
     
 ]
