8b06dc75508da3da9530a0bbb16b5e2e66d13b20
Fidiling <theblackpanthermarc@gmail.com>
2026-03-17 16:51:59 +0000

    added some views and urls for my team_feature

--- teams_feature/urls.py
+++ teams_feature/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('DepartmentID/', views.DepartmentID, name='DepartmentID'),
+    path('UpDownDependencies/', views.UpDownDependencies, name='UpDownDependencies'),
+    path('TeamPage/', views.TeamPage, name='TeamPage')
 ]
--- teams_feature/views.py
+++ teams_feature/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def DepartmentID(request):
+    return render(request, 'messages_/DepartmentID.html')
+
+def UpDownDependencies(request):
+    return render(request, 'UpDownDependencies.html')
+
+def TeamPage(request):
+    return render(request, 'TeamPage.html')
+
