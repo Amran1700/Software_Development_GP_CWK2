77f3a156e5f0ac114bd345b881aae754e172f8e9
Sadana Suresh <sadanasuresh001@gmail.com>
2026-03-23 10:32:12 +0000

    Remove unnecessary lines in apps.py

--- reports_feature/apps.py
+++ reports_feature/apps.py
@@ -1,8 +1,5 @@
-# reports/apps.py
 from django.apps import AppConfig
-
 
 class ReportsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reports'
-    verbose_name = 'Reports'
