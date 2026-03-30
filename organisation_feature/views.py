42a33b461a5406024ab07c4c7e190ad5cabfee8f
Ehsaan Zakriya <160361007+ehsaanzxk@users.noreply.github.com>
2026-03-30 16:16:24 +0100

    Refactor views to include org chart and department list

--- organisation_feature/views.py
+++ organisation_feature/views.py
@@ -1,13 +1,34 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Department, TeamType
 
 
-def home(request):
-    """Landing page until organisation routes are added."""
-    return HttpResponse(
-        "<h1>Sky Engineering</h1>"
-        "<p>Try "
-        '<a href="/messages/inbox/">Messages</a>, '
-        '<a href="/reports/">Reports</a>, or '
-        '<a href="/admin/">Admin</a>.'
-        "</p>"
-    )
+def org_chart(request):
+    """Displays the organisation chart showing all departments and their teams."""
+    departments = Department.objects.all()
+    return render(request, 'organisation_feature/org_chart.html', {'departments': departments})
+
+
+def department_list(request):
+    """Displays a list of all departments with optional search."""
+    query = request.GET.get('q', '')
+    if query:
+        departments = Department.objects.filter(
+            department_name__icontains=query
+        ) | Department.objects.filter(
+            specialisation__icontains=query
+        )
+    else:
+        departments = Department.objects.all()
+    return render(request, 'organisation_feature/department_list.html', {
+        'departments': departments,
+        'query': query,
+    })
+
+def department_detail(request, department_id):
+    """Displays the detail page for a single department."""
+    department = Department.objects.get(id=department_id)
+    team_types = TeamType.objects.all()
+    return render(request, 'organisation_feature/department_detail.html', {
+        'department': department,
+        'team_types': team_types,
+    })
