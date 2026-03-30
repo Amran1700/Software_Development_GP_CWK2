33ee7929edf2dea8a98222ae69fc45b7db10b883
Ehsaan Zakriya <160361007+ehsaanzxk@users.noreply.github.com>
2026-03-30 16:14:04 +0100

    Create TeamType and Department models
    Added TeamType and Department models with fields and ordering.

--- organisation_feature/models.py
+++ organisation_feature/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+# Register your models here.
 
-# Create your models here.
+class TeamType(models.Model):
+    #Represents a category that team belongs to
+
+    type_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.type_name
+    
+    class Meta: 
+        ordering = ['type_name']
+
+
+class Department (models.Model):
+    #reprents department in sky organisation 
+
+    department_name = models.CharField(max_length=150)
+    specialisation = models.CharField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.department_name
+    
+    class Meta:
+        ordering = ['department_name']
+
