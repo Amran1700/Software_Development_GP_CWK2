b051a1b2cddb79aa90ff02484351a27e26c8bf46
Ehsaan Zakriya <160361007+ehsaanzxk@users.noreply.github.com>
2026-03-30 16:15:57 +0100

    Add organization-related URL paths

--- organisation_feature/urls.py
+++ organisation_feature/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('org-chart/', views.org_chart, name='org_chart'),
+    path('departments/', views.department_list, name='department_list'),
+    path('departments/<int:department_id>/', views.department_detail, name='department_detail'),
 ]
