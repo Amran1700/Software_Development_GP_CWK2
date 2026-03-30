5e9de43aa8c43a79a869699017a77c230fdf82df
Ehsaan Zakriya <160361007+ehsaanzxk@users.noreply.github.com>
2026-03-30 16:15:12 +0100

    Update admin.py

--- organisation_feature/admin.py
+++ organisation_feature/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Department, TeamType
 
 # Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    #admin config for departmen model
+    list_display = ['department_name', 'specialisation']
+    search_fields= ['department_name', 'specialisation']
+
+@admin.register(TeamType)
+class TeamTypeAdmin(admin.ModelAdmin):
+    #Admin config for teamType model
+    list_display = ['type_name', 'description']
+    search_fields = ['type_name']
+    
+
