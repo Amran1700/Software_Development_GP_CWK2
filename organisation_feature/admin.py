5560c2c63fecea49c3469b8fc409e8caa234fba1
E <ehsaanzxk@Ehsaans-MacBook-Air-2.local>
2026-04-01 15:01:13 +0100

    Add docstrings and comments to admin

--- organisation_feature/admin.py
+++ organisation_feature/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from .models import Department, TeamType
 
-# Register your models here.
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    #admin config for departmen model
+    """
+    Admin configuration for the Department model.
+    Displays department name and specialisation in the list view.
+    Allows searching by department name and specialisation.
+    """
     list_display = ['department_name', 'specialisation']
-    search_fields= ['department_name', 'specialisation']
+    search_fields = ['department_name', 'specialisation']
+
 
 @admin.register(TeamType)
 class TeamTypeAdmin(admin.ModelAdmin):
-    #Admin config for teamType model
+    """
+    Admin configuration for the TeamType model.
+    Displays type name and description in the list view.
+    Allows searching by type name.
+    """
     list_display = ['type_name', 'description']
     search_fields = ['type_name']
-    
-
